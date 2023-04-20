3e495d6b6060c5477b2d2161c51fa9aa038798ad
ViktoriaGavrosh <viktoria.gavrosh@gmail.com>
2023-04-20 15:12:16 +0300

    fix "exit" - work

--- parking/Main.kt
+++ parking/Main.kt
@@ -5,6 +5,7 @@ fun main() {
     var lotParking: LotParking
     var message: List<String>
     while (true) {
+        if (numberSpots < 0) break
         lotParking = LotParking(numberSpots)
         message = Attendant(lotParking).talk()
         if (message[0] == "create") numberSpots = message[1].toInt() else break
@@ -15,7 +16,10 @@ private fun readNumberSpots(): Int {
     var listText: List<String>
     while (true) {
         listText = readln().split(" ")
-        if (listText[0] == "create") return listText[1].toInt()
+        when (listText[0]) {
+            "create" -> return listText[1].toInt()
+            "exit" -> return -1
+        }
         println("Sorry, a parking lot has not been created.")
     }
 }
